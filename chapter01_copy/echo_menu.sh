a9d94784624124e13c50b08e06d384a9fd554417
john.huang <john.huang@cloudwise.com>
2022-09-04 20:28:28 +0800

    chapter01_copy_start

--- chapter01_copy/echo_menu.sh
+++ chapter01_copy/echo_menu.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo ""
+#echo -e "\e[42m \tHello Shell World!!!\e[0m"
+echo -e "\e[32m \tHello Shell World!!!\e[0m"
+echo -e "\e[33m \tHello Shell World!!!\e[0m"
+echo -e "\e[34m \tHello Shell World!!!\e[0m"
+echo -e "\e[35m \tHello Shell World!!!\e[0m"
+
+#echo -e "\033[42m \tHello Shell Wodld!!!\033[0m"
