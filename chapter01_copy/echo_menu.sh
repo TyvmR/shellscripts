0a9840d26bdc2ff156dd26182a5059319216c87b
john.huang <john.huang@cloudwise.com>
2022-09-19 09:46:35 +0800

    cut down

--- chapter01_copy/echo_menu.sh
+++ chapter01_copy/echo_menu.sh
@@ -7,3 +7,8 @@ echo -e "\e[34m \tHello Shell World!!!\e[0m"
 echo -e "\e[35m \tHello Shell World!!!\e[0m"
 
 #echo -e "\033[42m \tHello Shell Wodld!!!\033[0m"
+
+
+echo -e "\033[42m-----------------------------\033[0m"
+echo -e "\e[2,10H 这里是菜单\t\t#"
+echo -e "\033[42m-----------------------------\033[0m"
